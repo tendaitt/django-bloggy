3368adf493b5d05f2c26347e0fec08e07dbe8b9b
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-18 08:48:15 +0200

    customize the model.

--- bloggy_project/blog/models.py
+++ bloggy_project/blog/models.py
@@ -4,3 +4,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
