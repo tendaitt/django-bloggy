21111a2082242eb3f9d08d3d134f2b603c0c073f
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-18 13:57:42 +0200

    display each individual post.

--- bloggy_project/blog/urls.py
+++ bloggy_project/blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<post_id>\d+)/$', views.post, name='post'),
 ]
