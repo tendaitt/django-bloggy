9f7b90e04416a16214f164c95d16462fac4482a1
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-18 14:08:31 +0200

    set up urls that are easier to read.

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -8,10 +8,12 @@ def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
     t = loader.get_template('blog/index.html')
     c = {'latest_posts': latest_posts, }
+    for post in latest_posts:
+        post.url = post.title.replace(' ', '_')
     return HttpResponse(t.render(c))
 
-def post(request, post_id):
-    single_post = get_object_or_404(Post, pk=post_id)
+def post(request, post_url):
+    single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
     t = loader.get_template('blog/post.html')
     c = {'single_post': single_post, }
     return HttpResponse(t.render(c))
