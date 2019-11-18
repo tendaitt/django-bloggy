9e8ff0f996c531ff484882032999e1f7aacd03b1
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-18 09:30:39 +0200

    add initial view for blog.

---
+++ bloggy_project/blog/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.template import Context, loader
+
+from blog.models import Post
+
+def index(request):
+    latest_posts = Post.objects.all().order_by('-created_at')
+    t = loader.get_template('blog/index.html')
+    c = {'latest_posts': latest_posts, }
+    return HttpResponse(t.render(c))
