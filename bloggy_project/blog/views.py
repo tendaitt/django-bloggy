d01e9417240a0cc3aef2d16d2fa77f361f77d3cd
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-20 05:12:28 +0200

    implement view count functionality.

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -14,6 +14,8 @@ def index(request):
 
 def post(request, post_url):
     single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
+    single_post.views += 1
+    single_post.save()
     t = loader.get_template('blog/post.html')
     c = {'single_post': single_post, }
     return HttpResponse(t.render(c))
