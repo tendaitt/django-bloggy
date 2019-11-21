b8ed4ef19a196563c70b951f1f2d4069254c7cdd
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-21 14:03:44 +0200

    fix popular post urls and add bootstrap grid system.

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -11,6 +11,8 @@ def index(request):
     c = {'latest_posts': latest_posts, 'popular_posts': popular_posts}
     for post in latest_posts:
         post.url = post.title.replace(' ', '_')
+    for popular_post in popular_posts:
+        popular_post.url = popular_post.title.replace(' ', '_')
     return HttpResponse(t.render(c))
 
 def post(request, post_url):
