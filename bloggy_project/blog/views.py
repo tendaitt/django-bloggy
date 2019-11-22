84cea17153a89e9e3fc107433a0bd95d46f66f6b
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-22 07:14:00 +0200

    ensure all posts have unique URLs.

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -5,32 +5,30 @@ from django.shortcuts import get_object_or_404, render, redirect
 from blog.models import Post
 from blog.forms import PostForm
 
-# helper function
-def encode_url(url):
-    return url.replace(' ', '_')
+####################
+# helper functions #
+####################
 
 def get_popular_posts():
     popular_posts = Post.objects.order_by('-views')[:5]
-    for popular_post in popular_posts:
-        popular_post.url = encode_url(popular_post.title)
     return popular_posts
+
+##################
+# view functions #
+##################
 
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
-    popular_posts = get_popular_posts()
     t = loader.get_template('blog/index.html')
-    c = {'latest_posts': latest_posts, 'popular_posts': popular_posts}
-    for post in latest_posts:
-        post.url = encode_url(post.title)
+    c = {'latest_posts': latest_posts, 'popular_posts': get_popular_posts()}
     return HttpResponse(t.render(c))
 
-def post(request, post_url):
-    single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
-    popular_posts = get_popular_posts()
+def post(request, slug):
+    single_post = get_object_or_404(Post, slug=slug)
     single_post.views += 1
     single_post.save()
     t = loader.get_template('blog/post.html')
-    c = {'single_post': single_post, 'popular_posts': popular_posts}
+    c = {'single_post': single_post, 'popular_posts': get_popular_posts()}
     return HttpResponse(t.render(c))
 
 def add_post(request):
