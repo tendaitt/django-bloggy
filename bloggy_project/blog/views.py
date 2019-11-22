4cc6198621f4bf006a783a6bf2dfdfebbe9ad265
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-22 05:51:52 +0200

    refactor code in views.py.

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -8,24 +8,26 @@ from blog.models import Post
 def encode_url(url):
     return url.replace(' ', '_')
 
+def get_popular_posts():
+    popular_posts = Post.objects.order_by('-views')[:5]
+    for popular_post in popular_posts:
+        popular_post.url = encode_url(popular_post.title)
+    return popular_posts
+
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
-    popular_posts = Post.objects.order_by('-views')[:5]
+    popular_posts = get_popular_posts()
     t = loader.get_template('blog/index.html')
     c = {'latest_posts': latest_posts, 'popular_posts': popular_posts}
     for post in latest_posts:
         post.url = encode_url(post.title)
-    for popular_post in popular_posts:
-        popular_post.url = encode_url(popular_post.title)
     return HttpResponse(t.render(c))
 
 def post(request, post_url):
     single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
-    popular_posts = Post.objects.order_by('-views')[:5]
+    popular_posts = get_popular_posts()
     single_post.views += 1
     single_post.save()
     t = loader.get_template('blog/post.html')
     c = {'single_post': single_post, 'popular_posts': popular_posts}
-    for popular_post in popular_posts:
-        popular_post.url = encode_url(popular_post.title)
     return HttpResponse(t.render(c))
