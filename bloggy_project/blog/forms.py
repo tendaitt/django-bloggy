5ee16ed913eab443b4b03f8773c5cc0fbbf25d9c
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-22 06:35:14 +0200

    remove view count from form field.

--- bloggy_project/blog/forms.py
+++ bloggy_project/blog/forms.py
@@ -5,4 +5,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tag', 'image', 'views']
+        fields = ['title', 'content', 'tag', 'image']
